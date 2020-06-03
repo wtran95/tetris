a614f72fdf2b6174f8b95b58b3e82cf4835cbb1b
Quan Tran <williamtran2207@gmail.com>
2020-06-03 10:41:18 -0400

    add gameOver

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ document.addEventListener('DOMContentLoaded',() => {
             draw();
             displayShape();
             addScore();
+            gameOver();
         }
     }
 
@@ -188,11 +189,16 @@ document.addEventListener('DOMContentLoaded',() => {
 
                 const squaresRemoved = squares.splice(i, width);
                 squares = squaresRemoved.concat(squares);
-                // squares.forEach(cell => grid.appendChild(cell));
+                squares.forEach(cell => grid.appendChild(cell));
 
             }
         }
     }
     //game over
-
+    function gameOver(){
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            scoreDisplay.innerHTML = 'end';
+            clearInterval(timerId);
+        }
+    }
 })
