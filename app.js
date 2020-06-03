662baaac6bfeaf4db7f300f0c9ced7f12455d363
Quan Tran <williamtran2207@gmail.com>
2020-06-02 23:04:23 -0400

    freeze tetrominoes at bottom

--- app.js
+++ app.js
@@ -5,4 +5,90 @@ document.addEventListener('DOMContentLoaded',() => {
     const startBtn = document.querySelector('#star-button');
     const width = 10;
 
+
+    const lTetromino = [
+        [1, width+1,width*2+1 ,2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ]
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+    //randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+    let current = theTetrominoes[random][currentRotation];
+
+    
+
+    //Draw the tetrominio
+    function draw(){
+        current.forEach(index => squares[currentPosition + index].classList.add('tetromino'));
+    }
+
+    function undraw(){
+        current.forEach(index => squares[currentPosition + index].classList.remove('tetromino'));
+    }
+
+    //assign function to keyCodes
+
+
+
+
+
+
+    //move down function
+    let timerId = setInterval(moveDown, 1000);
+
+    function moveDown(){
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    //freeze function
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+
+            random = Math.floor(Math.random()*theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        }
+    }
+
+    //move the tetromino left, unless is at the edge or there is a blockage
+
+
+    //move the tetromino right, unless is at the edge or there is a blockage
+
+
 })
