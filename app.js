ae0ab3defd80a2fd3df09116072485b5f910ba35
Quan Tran <williamtran2207@gmail.com>
2020-06-02 23:28:22 -0400

    Add tetrominio left and right navigation

--- app.js
+++ app.js
@@ -57,10 +57,20 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     //assign function to keyCodes
+    function control(e){
+        if(e.keyCode === 37){
+            moveLeft();
+        } else if(e.keyCode === 38){
+            // rotate();
+        } else if(e.keyCode === 39){
+            moveRight();
+        } else if(e.keyCode === 40){
+            moveDown();
+        }
+    }
 
-
-
-
+    //Listen to key pressed
+    document.addEventListener('keyup', control);
 
 
     //move down function
@@ -78,6 +88,7 @@ document.addEventListener('DOMContentLoaded',() => {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
             current.forEach(index => squares[currentPosition + index].classList.add('taken'));
 
+            //Reset
             random = Math.floor(Math.random()*theTetrominoes.length);
             current = theTetrominoes[random][currentRotation];
             currentPosition = 4;
@@ -86,9 +97,25 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     //move the tetromino left, unless is at the edge or there is a blockage
-
+    function moveLeft(){
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+        if(!isAtLeftEdge) currentPosition -= 1;
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition += 1;
+        }  
+        draw();
+    }
 
     //move the tetromino right, unless is at the edge or there is a blockage
-
+    function moveRight(){
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === (width-1));
+        if(!isAtRightEdge) currentPosition += 1;
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -= 1;
+        }
+        draw();
+    }
 
 })
