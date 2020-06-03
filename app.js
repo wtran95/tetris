94f489aee62f1003291046ed6b74ef6b01d1bd42
Quan Tran <williamtran2207@gmail.com>
2020-06-02 22:17:45 -0400

    Setting up project

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const grid = document.querySelector('.grid');
+    let squares = document.querySelectorAll('.grid div');
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#star-button');
+    const width = 10;
+
+})
